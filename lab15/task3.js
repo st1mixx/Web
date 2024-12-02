04e47076857851a2b3dc66d883906342de2f8e85
Sviatoslav Chirkov <sviatoslavchirkov@gmail.com>
2024-12-02 17:25:20 +0200

    Create task3.js

---
+++ lab15/task3.js
@@ -0,0 +1,13 @@
+let numb = 10000; 
+let result = numb; 
+let counter = 0; 
+
+
+while (result >= 50) {
+  result /= 2; 
+  counter++; 
+}
+
+
+console.log("Result:", result);
+console.log("Counter:", counter);
